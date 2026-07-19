8e3fe71a433067a122c0c4860ed95c803c3ad6a3
Klebonas <enrikas1010@gmail.com>
2026-07-19 13:58:53 +0300

    Update for cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,9 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+// Cloudflare next-on-pages dev setup
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
